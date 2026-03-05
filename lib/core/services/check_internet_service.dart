a6a30042b7b4105ee7d3eac5fd4a266e3b76eae7
Ahmed Ehab <ahmedehab56200@gmail.com>
2026-03-06 00:02:41 +0200

    create check internet service

--- lib/core/services/check_internet_service.dart
+++ lib/core/services/check_internet_service.dart
@@ -1,1 +1,13 @@
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 
+class CheckInternetService {
+  final InternetConnection internetConnection;
+  CheckInternetService({required this.internetConnection});
+  Future<bool> isConnected() async {
+    return internetConnection.hasInternetAccess;
+  }
+
+  Stream<InternetStatus> onInternetStatusChange() {
+    return internetConnection.onStatusChange;
+  }
+}
